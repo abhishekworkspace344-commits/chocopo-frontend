4c29a9c0e965fe2671391d4dafa9fadaad538453
Abhishek Krishna <abhishekworkspace344@gmail.com>
2026-08-01 10:51:01 +0530

    Update API URLs to production backend

--- src/components/SupportChat.js
+++ src/components/SupportChat.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 
-const API_BASE_URL = "http://127.0.0.1:5000/api";
+const API_BASE_URL = "https://chocopo-backend.onrender.com/api";
 
 export default function SupportChat() {
   const [isOpen, setIsOpen] = useState(false);
